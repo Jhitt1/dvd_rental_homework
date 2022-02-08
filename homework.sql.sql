168d6d9034cc9494ca70d08c073dcbeaa7ce27f0
Jesse Hitt <hittj77@gmail.com>
2022-02-08 09:01:23 -0600

    first commit

---
+++ homework.sql.sql
@@ -0,0 +1,71 @@
+--1--How many actors are there with the last name ‘Wahlberg’?
+
+--select *
+--from actor
+--where last_name like 'Wahlberg'
+
+--2--How many payments were made between $3.99 and $5.99?
+
+--select *
+--from payment
+--where amount between 3.99 and 5.99
+
+--3--What film does the store have the most of? (search in inventory)
+
+--select *
+--from 
+--where 
+
+
+--4--How many customers have the last name ‘William’?
+
+--select *
+--from customer
+--where last_name like 'Wil%'
+
+--5--What store employee (get the id) sold the most rentals?
+
+--select *
+--from payment
+--join staff on payment.staff_id = staff.staff_id 
+
+--6--How many different district names are there?
+
+--select * 
+--from country
+--where city
+
+--7--What film has the most actors in it? (use film_actor table and get film_id)
+
+--ELECT *
+--FROM film
+--LIMIT 5
+
+--8--From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
+
+
+--9--How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers
+--with ids between 380 and 430? (use group by and having > 250)
+
+
+--10--Within the film table, how many rating categories are there? And what rating has the most
+--movies total?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
